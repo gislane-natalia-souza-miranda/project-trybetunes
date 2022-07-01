b8c11a02bc99d62c3d7dd8cba5b07f530677d6bf
Gislane Natália de Souza Miranda <gislane.natalia.souza.miranda@gmail.com>
2022-07-01 17:59:50 -0300

    Requisito 2- Crie um formulário para identificação

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,81 @@
 // Rota /
 import React from 'react';
+import PropTypes from 'prop-types';
+import { createUser } from '../services/userAPI';
+import Loading from './Loading';
+
+const STATE_INITIAL = {
+  loading: false,
+  name: '',
+};
 
 class Login extends React.Component {
+  state = {
+    ...STATE_INITIAL,
+  };
+
+  // Pega o que foi digitado no input e atualiza o estado.
+  onInputChange = ({ target }) => {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value; // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/trim
+    this.setState({ [name]: value });
+  }
+
+  // verifica se button está habilitado com a restrição de ter mais de 3 caracteres.
+  isLoginButtonDisabled = () => {
+    const { name } = this.state;
+    const MINIMO_CARACTERES = 3;
+    return name.trim().length < MINIMO_CARACTERES;
+  }
+
+  onLoginButtonClick = () => {
+    this.setState(
+      (prevState) => ({ ...prevState, loading: true }),
+      async () => {
+        const { history } = this.props;
+        const { name } = this.state;
+        await createUser({ name });
+        history.push('/search');
+      },
+    );
+  }
+
   render() {
-    return (<div data-testid="page-login">Login</div>);
+    const { name, loading } = this.state;
+
+    if (!loading) {
+      return (
+        <div data-testid="page-login">
+          <forms onSubmit={ (event) => event.preventDefault() }>
+            {/* Parar submit do form: https://stackoverflow.com/a/39841238 */}
+
+            Nome:
+            <input
+              type="text"
+              value={ name }
+              onChange={ this.onInputChange }
+              name="name"
+              data-testid="login-name-input"
+            />
+
+            <button
+              type="submit"
+              disabled={ this.isLoginButtonDisabled() }
+              onClick={ this.onLoginButtonClick }
+              data-testid="login-submit-button"
+            >
+              Entrar
+            </button>
+
+          </forms>
+        </div>);
+    }
+    return <Loading />;
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.objectOf.isRequired,
+};
 
 export default Login;
